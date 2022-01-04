c8bb1e0c08ba790e5486eadcd60e33c21c1a1bf4
unknown <yasminewael60@gmail.com>
2022-01-05 00:20:09 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
+import Counter from './components/counter/Counter';
 function App() {
   return (
     <div>
+      <Counter/>
     </div>
   );
 }
