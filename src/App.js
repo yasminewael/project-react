3dba3c4e55a14e7d689163df2f0918ac538f3051
unknown <yasminewael60@gmail.com>
2022-02-04 18:46:44 +0200

    counter del

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Counter from "./components/counter/Counter";
+// import Counter from "./components/counter/Counter";
 import Shop from "./components/shop/Shop";
 import Product from "./components/product/Product";
 import Todo from "./components/todo/Todo";
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/counter" element={<Counter />} />
+          {/* <Route path="/counter" element={<Counter />} /> */}
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop/:id" element={<Product />} />
           <Route path="/todo" element={<Todo />} />
